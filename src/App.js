8a9086608e35da12ed1d44da4f3839cce6d2b865
sahlminkok <sahlminkok7@gmail.com>
2024-09-15 20:59:07 +0100

    Create components of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
     <div>
-      <p>Hello, World!</p>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😁 trip</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
   );
 }
 
