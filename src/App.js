191f0bfe9b0c4e17e75510a26ff726a5b214ba72
sahlminkok <sahlminkok7@gmail.com>
2024-09-22 16:37:23 +0100

    Calculate statistics in stats component

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         handleDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -109,10 +109,29 @@ function Item({ item, handleDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = (numPacked / numItems) * 100;
+
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding items to your packing list 🚀</em>
+      </footer>
+    );
+  }
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+      {percentage !== 100 ? (
+        <em>
+          👜 You have {numItems} items on your list, and you already packed{" "}
+          {numPacked} ({percentage.toPrecision(2)}%)
+        </em>
+      ) : (
+        <em>You've got everything, Ready to go ✈️</em>
+      )}
     </footer>
   );
 }
